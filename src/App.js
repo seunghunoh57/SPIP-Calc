c7fbce8ceeef91cbdf9374c78e54cde047bbe498
Seunghun Oh <seunghun.oh57@gmail.com>
2020-02-13 17:51:35 -0500

    Add feature that adds marker when user clicks on a coordinate on Maps, displays a polygon based upon the clicks

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import './App.css';
-import { Map, Marker, Polygon, GoogleApiWrapper } from 'google-maps-react';
-import { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
-import { api_key } from './config.js';
-import { AppAutocomplete } from './AppAutocomplete';
+import React from "react";
+import "./App.css";
+import { Map, Marker, Polygon, GoogleApiWrapper } from "google-maps-react";
+import { geocodeByAddress, getLatLng } from "react-places-autocomplete";
+import { api_key } from "./config.js";
+import { AppAutocomplete } from "./AppAutocomplete";
 
 const style = {
-  width: '80vw',
-  height: '80vh',
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-}
+  width: "80vw",
+  height: "80vh",
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)"
+};
 
 export class App extends React.Component {
   constructor(props) {
@@ -22,40 +22,56 @@ export class App extends React.Component {
         lat: null,
         lng: null
       },
-      polyCoords: [{lat: 42.3601, lng: -71.0589}],
-      address: "",
-    }
+      polyCoords: [
+        { lat: 42.3601, lng: -71.0589 },
+        { lat: 40.4173, lng: -82.9071 },
+        { lat: 36.7783, lng: -119.4179 }
+      ],
+      address: ""
+    };
   }
 
   handleChange = address => {
-    this.setState({address});
-  }
+    this.setState({ address });
+  };
 
   handleSelect = address => {
     geocodeByAddress(address)
       .then(results => getLatLng(results[0]))
       .then(latLng => {
-        console.log('Success', latLng);
-        this.setState({currentCoord: latLng});
+        console.log("Success", latLng);
+        this.setState({ currentCoord: latLng });
       })
-      .catch(error => console.error('Error while retrieving coordinates', error));
+      .catch(error =>
+        console.error("Error while retrieving coordinates", error)
+      );
   };
 
   placeMarker = coord => {
-    return <Marker
-      draggable
-      position={coord}
-    />
-  }
+    return <Marker draggable position={coord} />;
+  };
+
+  placeNewMarker = (mapProps, map, clickEvent) => {
+    console.log(
+      "Clicked coordinate: ",
+      clickEvent.latLng.lat(),
+      clickEvent.latLng.lng()
+    );
+
+    var coord = { lat: clickEvent.latLng.lat(), lng: clickEvent.latLng.lng() };
+    console.log(typeof coord.lat);
+    this.setState(prevState => ({
+      ...prevState,
+      polyCoords: prevState.polyCoords.concat(coord)
+    }));
+  };
 
   placePolyMarkers = () => {
-    return (
-      this.state.polyCoords.map(coord => {
-        console.log(coord);
-        return this.placeMarker(coord)
-      })
-    );
-  }
+    console.log(this.state.polyCoords);
+    return this.state.polyCoords.map(coord => {
+      return this.placeMarker(coord);
+    });
+  };
 
   render() {
     return (
@@ -74,11 +90,12 @@ export class App extends React.Component {
             center={this.state.currentCoord}
             google={this.props.google}
             onReady={this.fetchPlaces}
+            onClick={this.placeNewMarker}
             zoom={14}
             style={style}
           >
             {this.placePolyMarkers()}
-            <Polygon 
+            <Polygon
               paths={this.state.polyCoords}
               strokeColor="#FF0000"
               strokeOpacity={0.8}
@@ -91,6 +108,6 @@ export class App extends React.Component {
   }
 }
 
-export default GoogleApiWrapper ({
-  apiKey: (api_key)
-}) (App)
+export default GoogleApiWrapper({
+  apiKey: api_key
+})(App);
